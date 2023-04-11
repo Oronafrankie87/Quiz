77ae8792a5d2accc2fabd14085cd31413fa76531
Francisco Orona <oronafrankie87@gmail.com>
2023-04-11 14:44:51 -0600

    More questions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,116 @@
 var questions = [
   {
-    prompt: "How much wood would a Woodchuck chuck if a Woodchuck could chuck wood?",
-    options: ["<$12.32 worth>","<About 700 pounds>","<8 times the body weigh of a Mexican Jumping Frog from Sri Lanka>","<Slightly more than 3 branches>"],
-    answer:"<About 700 pounds>"
+    prompt:
+      "How much wood would a Woodchuck chuck if a Woodchuck could chuck wood?",
+    options: [
+      "<$12.32 worth>",
+      "<About 700 pounds>",
+      "<8 times the body weigh of a Mexican Jumping Frog from Sri Lanka>",
+      "<Slightly more than 3 branches>",
+    ],
+    answer: "<About 700 pounds>",
   },
   {
     prompt: "What is the correct answer to this question? Guess..",
-    options: ["<This one>","<4>","<None of these>","<Every Other Leap Year>"],
-    answer:"<This one>"
+    options: [
+      "<This one>",
+      "<4>",
+      "<None of these>",
+      "<Every Other Leap Year>",
+    ],
+    answer: "<This one>",
   },
   {
     prompt: "What are two things you can NEVER eat for breakfast?",
-    options: ["<Lasagna>","<Squirrels>","<Dinner>","<Hand Soap>"],
-    answer:"<Dinner>"
+    options: ["<Lasagna>", "<Squirrels>", "<Dinner>", "<Hand Soap>"],
+    answer: "<Dinner>",
   },
   {
     prompt: "What is always coming but will never arrive?",
-    options: ["<The end of COVID>","<Your lunch from Grubhub>","<Tomorrow>","<The of the Full Stack boot camp>"],
-    answer:"<Tomorrow>"
+    options: [
+      "<The end of COVID>",
+      "<Your lunch from Grubhub>",
+      "<Tomorrow>",
+      "<The of the Full Stack boot camp>",
+    ],
+    answer: "<Tomorrow>",
   },
   {
     prompt: "During an Eclipse, is it safe to stare directly at William Defoe?",
-    options: ["<Only with protective shades>","<Yes>","<As long as you have a burrito in one hand>","<Its never safe to stare directly at William Defoe>"],
-    answer:"<Its never sage to stare directly at William Defoe>"
+    options: [
+      "<Only with protective shades>",
+      "<Yes>",
+      "<As long as you have a burrito in one hand>",
+      "<Its never safe to stare directly at William Defoe>",
+    ],
+    answer: "<Its never sage to stare directly at William Defoe>",
   },
   {
     prompt: "Why?",
-    options: ["<What?>","<Because..>","<Blue>","<How?>"],
-    answer:"<Because..>"
+    options: ["<What?>", "<Because..>", "<Blue>", "<How?>"],
+    answer: "<Because..>",
   },
   {
-    prompt: "How many more stupid questions do I have to keep answering to get to the end of this stupid quiz?",
-    options: ["<1,487.49 more questions>","<π>","<I haven't answered any questions>","<One more, relax!>"],
-    answer:"<About 700 pounds>"
+    prompt:
+      "How many more stupid questions do I have to keep answering to get to the end of this stupid quiz?",
+    options: [
+      "<1,487.49 more questions>",
+      "<π>",
+      "<I haven't answered any questions>",
+      "<Four more>",
+    ],
+    answer: "<Four more>",
   },
   {
-    prompt: "What time was this question thought of?",
-    options: ["<$12.32 worth>","<About 700 pounds>","<8 times the body weigh of a Mexican Jumping Frog from Sri Lanka>","<Slightly more than 3 branches>"],
-    answer:"<About 700 pounds>"
-  }
-
-]
+    prompt: "What is the main ingredient in a unicorn sandwich?",
+    options: [
+      "<Butter>",
+      "<Asbestos>",
+      "<Flugengroggle seeds>",
+      "<A good nights sleep>",
+    ],
+    answer: "<A good night sleep>",
+  },
+  {
+    prompt: "When you are standing, why aren't your feet on top of your head?",
+    options: [
+      "<Because it would hurt>",
+      "<Because your standing on them stupid>",
+      "<Because of the way you look at yourself in the mirror>",
+      "<Because >",
+    ],
+    answer: "<Because your standing on them stupid>",
+  },
+  {
+    prompt: "Whats the difference between a Crocodile and an Alligator?",
+    options: [
+      "<One sees you later and the other sees you after a while>",
+      "<All Crocodiles are born Crocodiles but Alligators are born lizards and turn into Alligators when they turn 15 years old>",
+      "<If you split a Alligator in half it becomes a Partigator, when you split a Crocodile in half it just dies.>",
+      "<One starts with C and the other starts with A>",
+    ],
+    answer: "<One sees you later and the other sees you after a while>",
+  },
+  {
+    prompt:
+      "The singer Meatloaf who was popular in the 70s, 80s and 90s would do anything for love, except?",
+    options: [
+      "<Jump over a fully grown Armadillo>",
+      "<This>",
+      "<File my taxes early>",
+      "<That>",
+    ],
+    answer: "<That>",
+  },
+  {
+    prompt:
+      "What is the most effective way to stop a fully grown Danny DeVito from charging and headbutting you?",
+    options: [
+      "<You can't stop Danny DeVito>",
+      "<Jump over him>",
+      "<Throw him an oven roasted turkey leg>",
+      "<Yell, STOP as loud as you can",
+    ],
+    answer: "<Jump over him>",
+  },
+];
