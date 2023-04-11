ccfb3fe8dfd93f18897342eb3fb9f0a211811386
Francisco Orona <oronafrankie87@gmail.com>
2023-04-11 13:57:53 -0600

    Questions added

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+var questions = [
+  {
+    prompt: "How much wood would a Woodchuck chuck if a Woodchuck could chuck wood?",
+    options: ["<$12.32 worth>","<About 700 pounds>","<8 times the body weigh of a Mexican Jumping Frog from Sri Lanka>","<Slightly more than 3 branches>"],
+    answer:"<About 700 pounds>"
+  },
+  {
+    prompt: "What is the correct answer to this question? Guess..",
+    options: ["<This one>","<4>","<None of these>","<Every Other Leap Year>"],
+    answer:"<This one>"
+  },
+  {
+    prompt: "What are two things you can NEVER eat for breakfast?",
+    options: ["<Lasagna>","<Squirrels>","<Dinner>","<Hand Soap>"],
+    answer:"<Dinner>"
+  },
+  {
+    prompt: "What is always coming but will never arrive?",
+    options: ["<The end of COVID>","<Your lunch from Grubhub>","<Tomorrow>","<The of the Full Stack boot camp>"],
+    answer:"<Tomorrow>"
+  },
+  {
+    prompt: "During an Eclipse, is it safe to stare directly at William Defoe?",
+    options: ["<Only with protective shades>","<Yes>","<As long as you have a burrito in one hand>","<Its never safe to stare directly at William Defoe>"],
+    answer:"<Its never sage to stare directly at William Defoe>"
+  },
+  {
+    prompt: "Why?",
+    options: ["<What?>","<Because..>","<Blue>","<How?>"],
+    answer:"<Because..>"
+  },
+  {
+    prompt: "How many more stupid questions do I have to keep answering to get to the end of this stupid quiz?",
+    options: ["<1,487.49 more questions>","<π>","<I haven't answered any questions>","<One more, relax!>"],
+    answer:"<About 700 pounds>"
+  },
+  {
+    prompt: "What time was this question thought of?",
+    options: ["<$12.32 worth>","<About 700 pounds>","<8 times the body weigh of a Mexican Jumping Frog from Sri Lanka>","<Slightly more than 3 branches>"],
+    answer:"<About 700 pounds>"
+  }
+
+]
