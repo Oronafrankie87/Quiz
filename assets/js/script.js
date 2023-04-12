07bcae51d4a9ed521badc9475331c33912543ff1
Francisco Orona <oronafrankie87@gmail.com>
2023-04-12 13:58:53 -0600

    complete redo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,10 @@
-//List of Questions and Answers
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
-// const questionCounterText = document.getElementById('questionCounter');
-const scoreText = document.getElementById('score');
-const countdown = document.getElementById('timerCount');
-
-
-let timer = 0
-let timerCount =200
-let interval = setInterval(startTime, 1000);
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-// let questionCounter = 0;
-let availableQuestions = [];
-
 var questions = [
   {
     question:
       "How much wood would a Woodchuck chuck if a Woodchuck could chuck wood?",
     choice1: "$12.32 worth",
     choice2: "About 700 pounds",
-    choice3:
-      "8 times the body weigh of a Mexican Jumping Frog from Sri Lanka",
+    choice3: "8 times the body weigh of a Mexican Jumping Frog from Sri Lanka",
     choice4: "Slightly more than 3 branches",
     answer: 2,
   },
@@ -32,16 +14,7 @@ var questions = [
     choice2: "4",
     choice3: "None of these",
     choice4: "Every Other Leap Year",
-
     answer: 1,
-  },
-  {
-    question: "What are two things you can NEVER eat for breakfast?",
-    choice1: "Lasagna",
-    choice2: "Squirrels",
-    choice3: "Dinner",
-    choice4: "Hand Soap",
-    answer: 3,
   },
   {
     question: "What is always coming but will never arrive?",
@@ -69,13 +42,28 @@ var questions = [
     answer: 2,
   },
   {
-    question:
-      "When you are standing, why aren't your feet on top of your head?",
-    choice1: "Because it would hurt",
-    choice2: "Because your standing on them stupid",
-    choice3: "Because of the way you look at yourself in the mirror",
-    choice4: "To avoid athletes forehead",
-    answer: 2
+    question: "Two of the hardest words to say are sorry and...?",
+    choice1: "Worcestershire sauce",
+    choice2: "Bamboozled",
+    choice3: "Hippopotomonstrosesquippedaliophobia",
+    choice4: "Rural",
+    answer: 1,
+  },
+  {
+    question: "What are you actually doing when you wash your hands?",
+    choice1: "Break dancing",
+    choice2: "Cleaning you top feet",
+    choice3: "Supervising your hands while they commit bacterial genocide.",
+    choice4: "Making Soap disappear",
+    answer: 3,
+  },
+  {
+    question: "When doing the Hokey Pokey, what is it all about?",
+    choice1: "Playing leapfrog with a stranger in a Taco Bell parking lot.",
+    choice2: "Lolly Gaging",
+    choice3: "Putting your right hand in, then out, then shaking it all about.",
+    choice4: "Playing Amazing Graze with your nose.",
+    answer: 3,
   },
   {
     question: "Whats the difference between a Crocodile and an Alligator?",
@@ -108,138 +96,10 @@ var questions = [
     question:
       "What is the American Dental Association's recommended way of entering a Chipotle?",
     choice1: "Through the doggie door",
-    choice2:
-      "Wrapping yourself in bubble wrap and jumping through the window>",
+    choice2: "Wrapping yourself in bubble wrap and jumping through the window>",
     choice3: "Hide inside a bag of rice then jump out before they open it",
     choice4: "'Kramer' in",
     answer: 4,
   },
 ];
 
-const CORRECT_BONUS = 1;
-const MAX_QUESTIONS = 11;
-
-
-
-startGame = () => {
-  timerCount = 200;
-  questionCounter = 0;
-  score = 0;
-  availableQuestions = [...questions];
-  startTime();
-  getNewQuestion();
-};
-
-function startTime() {
-  timer++;
-  countdown.innerHTML = convertSeconds(timerCount - timer);
-  if (timer == timerCount) {
-    clearInterval(interval);
-
-    itAlive()
-  }
-}
-
-function itAlive() {
-  return window.location.assign("/The-Nonsense-Quiz/game.html")
-}
-
-getNewQuestion = () => {
-    if(availableQuestions.length ===0 || questionCounter >= MAX_QUESTIONS) {
-    localStorage.setItem('mostRecentScore', score);
-    return window.location.assign("/end.html");
-   }
-  // questionCounter++;
-
-  currentQuestion = availableQuestions[questionCounter];
-  question.innerText = currentQuestion.question;
-
-
-  // const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-  // currentQuestion = availableQuestions[questionIndex];
-  // question.innerText = currentQuestion.question;
-
-  choices.forEach((choice) => {
-    const number = choice.dataset.number;
-    choice.innerText = currentQuestion["choice" + number];
-
-  });
-
-  // availableQuestions.splice(questionIndex, 1);
-
-  questionCounter++;
-  acceptingAnswers = true;
-
-};
-
-const nextButton = document.getElementById("nextButton");
-nextButton.addEventListener("click", () => {
-  getNewQuestion();
-});
-
-// getNextQuestion = () => {
-//   questionCounter++;
-//   currentQuestion = questions[questionCounter - 1]
-//   question.innerText = currentQuestion.question;
-// }
-
-choices.forEach(choice => {
-  choice.addEventListener("click", e => {
-    if (!acceptingAnswers) return;
-    acceptingAnswers = false;
-    const selectedChoice = e.target;
-    const selectedAnswer = selectedChoice.dadaset["number"];
-
-    const classToApply =
-      selectedAnswer == currentQuestion.answer ? 'correct' : "incorrect";
-      if(classToApply === 'correct') {
-        incrementScore(CORRECT_BONUS);
-      }
-
-
-    selectedChoice.parentElement.classList.add(classToApply);
-
-    setTimeout( () => {
-      selectedChoice.parentElement.classList.remove(classToApply);
-      getNewQuestion();
-    }, 1000);
-  });
-});  
-  // if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
-  //   localStorage.setItem('mostRecentScore', score);
-  //   return window.location.assign("/end.html");
-  // }
-  // getNextQuestion();
-
-
-
-// choice.addEventListener("click", e => {
-//   if (!acceptingAnswers) return;
-// });
-  //   acceptingAnswers = false;
-  //   const selectedChoice = e.target;
-  //   const selectedAnswer = selectedChoice.dataset["number"];
-
-  //   const classToApply = 
-  //     selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-   
-  //   if(classToApply === 'correct') {
-  //     incrementScore(CORRECT_BONUS);
-  //   }
-
-  //   selectedChoice.parentElement.classList.add(classToApply);
-
-  //   setTimeout( () => {
-  //     selectedChoice.parentElement.classList.remove(classToApply);
-  //     getNewQuestion();
-  //   },  1000);
-
-//   });
-// })
-
-incrementScore = num => {
-    score += num;
-    scoreText.innerText = score;
-}
-
-startGame();
