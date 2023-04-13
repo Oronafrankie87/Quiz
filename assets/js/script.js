a71e9cf8827bc454821f4f505062bd0ab1afd28c
Francisco Orona <oronafrankie87@gmail.com>
2023-04-12 20:11:14 -0600

    Styling and JavaScript updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,138 +1,65 @@
 const questions = [
   {
-    question:
-      "How much wood would a Woodchuck chuck if a Woodchuck could chuck wood?",
-    choice: [
-      "a. $12.32 worth",
-      "b. About 700 pounds",
-      "c. 8 times the body weigh of a Mexican Jumping Frog from Sri Lanka",
-      "d. Slightly more than 3 branches",
-    ],
+    question: "How much wood would a Woodchuck chuck if a Woodchuck could chuck wood?",
+    choices: ["a. $12.32 worth","b. About 700 pounds", "c. 8 times the body weigh of a Mexican Jumping Frog from Sri Lanka","d. Slightly more than 3 branches"],
     answer: "b. About 700 pounds",
   },
   {
-    question: "What is the correct answer to this question? Guess..",
-    choice: [
-      "a. This one",
-      "b. 4",
-      "c. None of these",
-      "d. Every Other Leap Year",
-    ],
-    answer: "a. This one",
-  },
-  {
     question: "What is always coming but will never arrive?",
-    choice: [
-      "a. The end of COVID",
-      "b. Your lunch from Grubhub",
-      "c. Tomorrow",
-      "d. The new Beetlejuice movie",
-    ],
+    choices: ["a. The end of COVID","b. Your lunch from Grubhub","c. Tomorrow","d. The new Beetlejuice movie"],
     answer: "c. Tomorrow",
   },
   {
-    question:
-      "During an Eclipse, is it safe to stare directly at William Defoe?",
-    choice: [
-      "a. Only with protective shades",
-      "b. Yes",
-      "c. As long as you have a burrito in one hand",
-      "d. Its never safe to stare directly at William Defoe",
-    ],
+    question: "During an Eclipse, is it safe to stare directly at William Defoe?",
+    choices: ["a. Only with protective shades","b. Yes","c. As long as you have a burrito in one hand","d. Its never safe to stare directly at William Defoe"],
     answer: "d. Its never safe to stare directly at William Defoe",
   },
   {
     question: "What is the main ingredient in a unicorn sandwich?",
-    choice: [
-      "a. A good nights sleep",
-      "b. Butter",
-      "c. Asbestos",
-      "d. Flugengroggle extract",
-    ],
+    choices: ["a. A good nights sleep","b. Butter","c. Asbestos","d. Flugengroggle extract"],
     answer: "b. Butter",
   },
   {
     question: "Two of the hardest words to say are sorry and...?",
-    choice: [
-      "a. Worcestershire sauce",
-      "b. Bamboozled",
-      "c. Hippopotomonstrosesquippedaliophobia",
-      "d. Rural",
-    ],
+    choices: ["a. Worcestershire sauce","b. Bamboozled","c. Hippopotomonstrosesquippedaliophobia","d. Rural"],
     answer: "a. Worcestershire sauce",
   },
   {
     question: "What are you actually doing when you wash your hands?",
-    choice: [
-      "a. Break dancing",
-      "b. Cleaning your top feet",
-      "c. Supervising your hands while they commit bacterial genocide.",
-      "d. Making Soap disappear",
-    ],
+    choices: ["a. Break dancing","b. Cleaning your top feet","c. Supervising your hands while they commit bacterial genocide.","d. Making Soap disappear"],
     answer: "c. Supervising your hands while they commit bacterial genocide.",
   },
   {
     question: "When doing the Hokey Pokey, what is it all about?",
-    choice: [
-      "a. Playing leapfrog with a stranger in a Taco Bell parking lot.",
-      "b. Lolly Gaging",
-      "c. Putting your right hand in, then out, then shaking it all about.",
-      "d. Playing Amazing Graze with your nose.",
-    ],
-    answer:
-      "c Putting your right hand in, then out, then shaking it all about.",
+    choices: ["a. Playing leapfrog with a stranger in a Taco Bell parking lot.","b. Lolly Gaging","c. Putting your right hand in, then out, then shaking it all about.","d. Playing Amazing Graze with your nose."],
+    answer:"c Putting your right hand in, then out, then shaking it all about.",
   },
   {
     question: "Whats the difference between a Crocodile and an Alligator?",
-    choice: [
-      "a. One sees you later and the other sees you after a while",
-      "b. All Crocodiles are born Crocodiles but Alligators are born lizards and turn into Alligators when they turn 15 years old",
-      "c. If you split an Alligator in half it becomes a Partigator, when you split a Crocodile in half it just dies.",
-      "d. One starts with C and the other starts with A",
-    ],
+    choices: ["a. One sees you later and the other sees you after a while","b. All Crocodiles are born Crocodiles but Alligators are born lizards and turn into Alligators when they turn 15 years old","c. If you split an Alligator in half it becomes a Partigator, when you split a Crocodile in half it just dies.","d. One starts with C and the other starts with A"],
     answer: "a. One sees you later and the other sees you after a while",
   },
   {
     question: "The singer Meatloaf would do anything for love, except?",
-    choice: [
-      "a. Carry on his Wayward son",
-      "b. This",
-      "c. That",
-      "d. File his taxes early",
-    ],
+    choices: ["a. Carry on his Wayward son","b. This","c. That","d. File his taxes early"],
     answer: "c. That",
   },
   {
     question:
       "What is the most effective way to stop a fully grown Danny DeVito from charging and headbutting you?",
-    choice: [
-      "a. You can't stop Danny DeVito",
-      "b. Jump over him",
-      "c. Throw him an oven roasted turkey leg",
-      "d. Yell, STOP as loud as you can sign language",
-    ],
+    choices: ["a. You can't stop Danny DeVito","b. Jump over him","c. Throw him an oven roasted turkey leg","d. Yell, STOP as loud as you can sign language"],
     answer: "b. Jump over him",
   },
   {
     question:
       "What is the American Dental Association's recommended way of entering a Chipotle?",
-    choice: [
-      "a. Through the doggie door",
-      "b. Wrapping yourself in bubble wrap and jumping through the window",
-      "c. Hide inside a bag of rice then jump out before they open it",
-      "d. 'Kramer' in",
-    ],
+    choices: ["a. Through the doggie door","b. Wrapping yourself in bubble wrap and jumping through the window","c. Hide inside a bag of rice then jump out before they open it","d. 'Kramer' in"],
     answer: "d. 'Kramer' in",
   },
   {
     question:
       "Barry Gibb, the oldest and only living member of the great singing group the Bee Gees is very good at?",
-    choice: [
-      "a. Jive Talkin'",
-      "b. Getting that night fever",
-      "c. Stayin' Alive",
-      "d. Mending a broken heart",
-    ],
+    choices: ["a. Jive Talkin'","b. Getting that night fever","c. Stayin' Alive","d. Mending a broken heart"],
     answer: "c. Stayin' Alive",
   },
 ];
@@ -161,50 +88,44 @@ var clearHighScoreBtn = document.getElementById("clearHighScoreBtn");
 var viewHighScore = document.getElementById("viewHighScore");
 var listOfHighScores = document.getElementById("listOfHighScores");
 
+startQuizBtn.addEventListener("click", newQuiz);
+
 // Other variables defined
 var correctAns = 0;
 var questionNum = 0;
 var scoreResult;
 var questionIndex = 0;
-var timeLeft = 151;
 
-startQuizBtn.addEventListener("click", newQuiz);
 
-submitInitialBtn.addEventListener("click", function (event) {
-  storeHighScores(event);
-});
+var totalTime = 151;
 
-viewHighScore.addEventListener("click", function (event) {
-  showHighScores(event);
-});
 
-goBackBtn.addEventListener("click", function () {
-  startDiv.style.display = "block";
-  highScoreSection.style.display = "none";
-});
 
 function newQuiz() {
   questionIndex = 0;
-  timeLeft = 150;
-  timeLeft.textContent = timeLeft;
+  totalTime = 150;
+  timeLeft.textContent = totalTime;
   initialInput.textContent = "";
+
   startDiv.style.display = "none";
   questionDiv.style.display = "block";
   timer.style.display = "block";
   timesUp.style.display = "none";
 
   var startTimer = setInterval(function () {
-    timeLeft--;
-    timeLeft.textContent = timeLeft;
-    if (timeLeft <= 0) {
+    totalTime--;
+    timeLeft.textContent = totalTime;
+    if (totalTime <= 0) {
       clearInterval(startTimer);
       if (questionIndex < questions.length - 1) {
         gameOver();
       }
     }
   }, 1000);
+
   showQuiz();
 }
+
 
 function showQuiz() {
   nextQuestion();
@@ -212,10 +133,10 @@ function showQuiz() {
 
 function nextQuestion() {
   questionTitle.textContent = questions[questionIndex].question;
-  choiceA.textContent = questions[questionIndex].choice[0];
-  choiceB.textContent = questions[questionIndex].choice[1];
-  choiceC.textContent = questions[questionIndex].choice[2];
-  choiceD.textContent = questions[questionIndex].choice[3];
+  choiceA.textContent = questions[questionIndex].choices[0];
+  choiceB.textContent = questions[questionIndex].choices[1];
+  choiceC.textContent = questions[questionIndex].choices[2];
+  choiceD.textContent = questions[questionIndex].choices[3];
 }
 
 function checkAnswer(answer) {
@@ -224,112 +145,126 @@ function checkAnswer(answer) {
   answerCheck.style.display = "block";
   if (
     questions[questionIndex].answer === questions[questionIndex].choices[answer]
-  ) {
-    correctAns++;
-    answerCheck.textContent = "Correct!";
-  } else {
-    totalTime -= 10;
-    timeLeft.textContent = totalTime;
-    answerCheck.textContent =
-      "Wrong! The correct answer is: " + questions[questionIndex].answer;
+    ) {
+      correctAns++;
+      answerCheck.textContent = "Correct!";
+    } else {
+      timeLeft -= 25;
+      timeLeft.textContent = totalTime;
+      answerCheck.textContent =
+      "Wrong!";
+    }
+    questionIndex++;
+    if (questionIndex < questions.length) {
+      nextQuestion();
+    } else {
+      gameOver();
+    }
   }
-  questionIndex++;
-  if (questionIndex < questions.length) {
-    nextQuestion();
-  } else {
-    gameOver();
+  
+  function chooseA() {
+    checkAnswer(0);
   }
-}
-
-function chooseA() {
-  checkAnswer(0);
-}
-choiceA.addEventListener("click", chooseA);
-
-function chooseB() {
-  checkAnswer(1);
-}
-choiceB.addEventListener("click", chooseB);
-
-function chooseC() {
-  checkAnswer(2);
-}
-choiceC.addEventListener("click", chooseC);
-
-function chooseD() {
-  checkAnswer(3);
-}
-choiceD.addEventListener("click", chooseD);
-
-function gameOver() {
-  summary.style.display = "block";
-  questionDiv.style.display = "none";
-  startDiv.style.display = "none";
-  timer.style.display = "none";
-  timesUp.style.display = "block";
-  finalScore.textContent = correctAns;
-}
-
-function storeHighScores(event) {
-  event.preventDefault();
-
-  if (initialInput.value === "") {
-    alert("Please enter your initials!");
-    return;
+  
+  function chooseB() {
+    checkAnswer(1);
   }
-
-  startDiv.style.display = "none";
-  timer.style.display = "none";
-  timesUp.style.display = "none";
-  summary.style.display = "none";
-  highScoreSection.style.display = "block";
-
-  var savedHighScores = localStorage.getItem("high scores");
-  var scoresArray;
-
-  if (savedHighScores === null) {
-    scoresArray = [];
-  } else {
-    scoresArray = JSON.parse(savedHighScores);
+  
+  function chooseC() {
+    checkAnswer(2);
   }
-
-  var userScore = {
-    initials: initialInput.value,
-    score: finalScore.textContent,
-  };
-
-  scoresArray.push(userScore);
-  var scoresArrayString = JSON.stringify(scoresArray);
-  window.localStorage.setItem("high scores", scoresArrayString);
-
-  showHighScores();
-}
-
-function showHighScores() {
-  startDiv.style.display = "none";
-  timer.style.display = "none";
-  questionDiv.style.display = "none";
-  timesUp.style.display = "none";
-  summary.style.display = "none";
-  highScoreSection.style.display = "block";
-  var savedHighScores = localStorage.getItem("high scores");
-  if (savedHighScores === null) {
-    return;
+  
+  function chooseD() {
+    checkAnswer(3);
   }
-  var storedHighScores = JSON.parse(savedHighScores);
-  for (var index = 0; index < storedHighScores.length; index++) {
-    var eachNewHighScore = document.createElement("p");
-    eachNewHighScore.innerHTML =
-      storedHighScores[index].initials + ": " + storedHighScores[index].score;
-    listOfHighScores.appendChild(eachNewHighScore);
+  
+  function gameOver() {
+    summary.style.display = "block";
+    questionDiv.style.display = "none";
+    startDiv.style.display = "none";
+    timer.style.display = "none";
+    timesUp.style.display = "block";
+    finalScore.textContent = correctAns;
   }
-}
-
-clearHighScoreBtn.addEventListener("click", function () {
-  window.localStorage.removeItem("high scores");
-  listOfHighScores.innerHTML = "High Scores Cleared!";
-  listOfHighScores.setAttribute(
-    "style",
-    "font-family: 'Archivo', sans-serif; font-style: italic;"
-  );
-});
+  
+  function storeHighScores(event) {
+    event.preventDefault();
+    
+    if (initialInput.value === "") {
+      alert("Please enter your initials!");
+      return;
+    }
+    
+    startDiv.style.display = "none";
+    timer.style.display = "none";
+    timesUp.style.display = "none";
+    summary.style.display = "none";
+    highScoreSection.style.display = "block";
+    
+    var savedHighScores = localStorage.getItem("high scores");
+    var scoresArray;
+    
+    if (savedHighScores === null) {
+      scoresArray = [];
+    } else {
+      scoresArray = JSON.parse(savedHighScores);
+    }
+    
+    var userScore = {
+      initials: initialInput.value,
+      score: finalScore.textContent,
+    };
+    
+    scoresArray.push(userScore);
+    var scoresArrayString = JSON.stringify(scoresArray);
+    window.localStorage.setItem("high scores", scoresArrayString);
+    
+    showHighScores();
+  }
+  var f = 0;
+  function showHighScores() {
+    startDiv.style.display = "none";
+    timer.style.display = "none";
+    questionDiv.style.display = "none";
+    timesUp.style.display = "none";
+    summary.style.display = "none";
+    highScoreSection.style.display = "block";
+    
+    var savedHighScores = localStorage.getItem("high scores");
+    if (savedHighScores === null) {
+      return;
+    }
+    var storedHighScores = JSON.parse(savedHighScores);
+    for (; f < storedHighScores.length; f++) {
+      var eachNewHighScore = document.createElement("p");
+      eachNewHighScore.innerHTML =
+      storedHighScores[f].initials + ": " + storedHighScores[f].score;
+      listOfHighScores.appendChild(eachNewHighScore);
+    }
+  }
+  choiceA.addEventListener("click", chooseA);
+  choiceB.addEventListener("click", chooseB);
+  choiceC.addEventListener("click", chooseC);
+  choiceD.addEventListener("click", chooseD);
+  
+  submitInitialBtn.addEventListener("click", function (event) {
+    storeHighScores(event);
+  });
+  
+  viewHighScore.addEventListener("click", function (event) {
+    showHighScores(event);
+  });
+  
+  goBackBtn.addEventListener("click", function () {
+    startDiv.style.display = "block";
+    highScoreSection.style.display = "none";
+  });
+  
+  clearHighScoreBtn.addEventListener("click", function () {
+    window.localStorage.removeItem("high scores");
+    listOfHighScores.innerHTML = "High Scores Cleared!";
+    listOfHighScores.setAttribute(
+      "style",
+      "font-family: 'Archivo', sans-serif; font-style: italic;"
+      );
+    });
